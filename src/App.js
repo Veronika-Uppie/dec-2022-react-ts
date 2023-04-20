cdc07a7cba5a2a85f1d89c35e5b68b2932425abf
Veronika-Uppie <veronikauppie@gmail.com>
2023-04-20 17:58:22 +0200

    lesson4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
-import logo from './logo.svg';
+// реалізувати 3 маршрути
+// todos - при переході на який тягнуться всі todo з https://jsonplaceholder.typicode.com/todos
+//
+//     albums - при переході на який тягнуться всі альбоми з https://jsonplaceholder.typicode.com/albums
+//
+//     comments - при переході на який тягнуться всі комментарі https://jsonplaceholder.typicode.com/comments
+//     при натисканні на комментар тягнеться пост, до якого належіить цей коментар. приклад запиту https://jsonplaceholder.typicode.com/posts/ID
+//     id поста взяти з коментаря (postId)
+//
+// відображати ті чи інші маршрути можна на будь-якому рівні на ваш вибір.
+
 import './App.css';
+import {Link, Route, Routes} from "react-router-dom";
+import Todo from "./components/todo/Todo";
+import Layout from "./components/layout/Layout";
+import About from "./components/about/About";
+import Home from "./components/home/Home";
 
 function App() {
   return (
-    <div>
+      <div>
+        <div>
+          <h2>menu</h2>
+          <ul>
+            <li>
+              <Link to={'/'}>home</Link>
+            </li>
+            <li>
+              <Link to={'/layout'}>layout</Link>
+            </li>
+            <li>
+              <Link to={'/about'}>about</Link>
+            </li>
+          </ul>
+        </div>
 
-    </div>
+        <div>
+          <h2>view</h2>
+          <Routes>
+
+            <Route path={'/'} element={<Home/>}/>>
+            <Route path={'/layout'} element={<Layout/>}>>
+              <Route path={'todo'} element={<Todo/>}/>
+                </Route>
+            <Route path={'/about'} element={<About/>}/>>
+
+          </Routes>
+        </div>
+
+      </div>
   );
 }
 
