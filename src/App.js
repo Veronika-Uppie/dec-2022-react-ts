e8703d6b1968263ac445f63fa925cfca181b72c8
Veronika-Uppie <veronikauppie@gmail.com>
2023-04-20 14:16:17 +0200

    hw4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
+// еще сделай еще небольшую правку в lesson4:
+//
+//     реалізувати 3 маршрути
+// todos - при переході на який тягнуться всі todo з https://jsonplaceholder.typicode.com/todos
+//
+//     albums - при переході на який тягнуться всі альбоми з https://jsonplaceholder.typicode.com/albums
+//
+//     comments - при переході на який тягнуться всі комментарі https://jsonplaceholder.typicode.com/comments
+//     при натисканні на комментар тягнеться пост, до якого належіить цей коментар. приклад запиту https://jsonplaceholder.typicode.com/posts/ID
+//     id поста взяти з коментаря (postId)
+//
+// відображати ті чи інші маршрути можна на будь-якому рівні на ваш вибір.
 import './App.css';
 
 function App() {
   return (
     <div>
+      <div>
+          <h2>menu</h2>
+          <ul>
+              <li>
+              </li>
+          </ul>
 
+
+      </div>
     </div>
   );
 }
